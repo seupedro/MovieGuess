ec3d94ef31bdbe1e1f5c3ed542cf29488579134a
nakamoto <hartmnn.p@gmail.com>
2018-06-08 11:22:51 -0300

    start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,36 @@
 package com.company;
 
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+
+    static List<String> movies = new ArrayList<>();
 
     public static void main(String[] args) throws FileNotFoundException {
 	// write your code here
 
-        Movie movie = new Movie();
-        movie.setMovieFile("movies.txt");
+       File file = new File("movies.txt");
+       Scanner scanner = new Scanner(file);
 
-        System.out.println(movie.getMovieListSize());
+
+       while (scanner.hasNextLine()){
+           movies.add(scanner.nextLine());
+       }
+
+       System.out.println(getMovie(0));
+       System.out.println(getRandomMovie());
+    }
+
+    private static String getMovie(int position){
+        return movies.get(position);
+    }
+
+    private static String getRandomMovie(){
+        return movies.get((int) (Math.random() * movies.size()));
     }
 }
